08de84adc3a80c3ada0a19d57fa6cf5dc02442da
Mikael <mikael.thorman@gmail.com>
2020-06-26 14:01:43 +0200

    cleanup

---
+++ hello-world/example-lambda-nodejs/zip-ggs-module.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z.exe" a ggs-lambda-nodejs.zip index.js
+"C:\Program Files\7-Zip\7z.exe" a ggs-lambda-nodejs.zip package.json
+"C:\Program Files\7-Zip\7z.exe" a ggs-lambda-nodejs.zip package-lock.json
+"C:\Program Files\7-Zip\7z.exe" a ggs-lambda-nodejs.zip .\node_modules
