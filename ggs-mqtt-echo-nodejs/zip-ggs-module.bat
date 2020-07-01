ebf119c45f58aeabd5eef15724d6caf7ade31206
Mikael <mikael.thorman@gmail.com>
2020-07-01 13:45:48 +0200

    renamed echo folder name

---
+++ ggs-mqtt-echo-nodejs/zip-ggs-module.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_echo_nodejs.zip index.js
+"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_echo_nodejs.zip package.json
+"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_echo_nodejs.zip package-lock.json
+"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_echo_nodejs.zip .\node_modules
--- ggs-mqtt-listen-lambda-nodejs/zip-ggs-module.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_listen_nodejs.zip index.js
-"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_listen_nodejs.zip package.json
-"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_listen_nodejs.zip package-lock.json
-"C:\Program Files\7-Zip\7z.exe" a ggs_mqtt_listen_nodejs.zip .\node_modules
